5a3968d399b8fb831383e71d2c64e289947faba7
JG <ludwing.gomez@epn.edu.ec>
2023-03-01 22:07:24 -0500

    feat: reporte mejorado

--- components/GenerarPdf.tsx
+++ components/GenerarPdf.tsx
@@ -7,29 +7,28 @@ export default function GeneratePDF({ recomendacion: recomendaciones }) {
 
     function generate() {
         const doc = new jsPDF('l', 'pt', 'a4');
-        doc.setFontSize(9)
+        doc.setFontSize(30)
         doc.setTextColor('#636363')
         doc.setFont('helvetica', 'normal')
         doc.text('RECOMENDACIONES',450,30,{
             align: 'center',
             maxWidth:800
         });
-        doc.text(recomendaciones,10,50,{
-            align: 'justify',
-            maxWidth:800,
-            lineHeightFactor:2
-        });
-        //doc.
-        /*autoTable(doc,{
-            head: [['RECOMENDACIONES']],
-            body:
-                person.forEach(({elemento}) => {
-                    console.log(elemento)
+        const numero= recomendaciones.length;
+        console.log(numero)
+
+        autoTable(doc, {
+
+            head: [['','','','','','','']],
+            body: [
+                recomendaciones.map((element) => {
                     return [
-                        [elemento]
+                        element
                     ]
-                }),
-        })*/
+                })
+                // ...
+            ],
+        })
         doc.save('recomendacionesSGI.pdf');
     }
     return (
